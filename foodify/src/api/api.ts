97920942dbbd2292cd60c5155df094fa493f9f58
pritamscodee <codewithpritam29@gmail.com>
2026-04-29 12:29:44 +0530

    b

--- foodify/src/api/api.ts
+++ foodify/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL:'https://express4foodify.onrender.com' 
 });
 
 api.interceptors.request.use(
